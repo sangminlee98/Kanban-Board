e400f9b09680bf8391f0bea66790ba5ea9cf18c5
sangminlee98 <tkdals0975@naver.com>
2022-04-19 22:26:43 +0900

    initialize project

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: 'black',
+  textColor: 'black',
+  accentColor: 'black',
+  cardBgColor: 'black',
+}
