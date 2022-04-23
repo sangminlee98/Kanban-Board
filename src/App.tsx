d767f3a60b0151f3370d6b32c817c2809ac2bb45
sangminlee98 <tkdals0975@naver.com>
2022-04-23 19:15:50 +0900

    Styling & placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,59 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed, 
-  figure, figcaption, footer, header, hgroup, 
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure, 
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-  body {
-    line-height: 1;
-  }
-  ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    font-family: 'Source Sans Pro', sans-serif;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  li {
-    margin-bottom: 10px;
-    font-size: 1.5rem;
-  }
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(3, 1fr);
+`;
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+const Card = styled.div`
+  background-color: ${props => props.theme.cardColor};
+  border-radius: 5px;
+  padding: 10px 10px;
+  margin-bottom: 5px;
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
   const onDragEnd = () => {}
   return (
-    <>
-      <GlobalStyle/>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
           <Droppable droppableId='one'>
-            {(magic) => 
-            <ul ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId='first' index={0}>
-                {(magic) => (<li ref={magic.innerRef} {...magic.draggableProps}>
-                  <span {...magic.dragHandleProps}>🔥</span>
-                  One
-                  </li>)}
-              </Draggable>
-              <Draggable draggableId='second' index={1}>
-                {(magic) => (<li ref={magic.innerRef} {...magic.draggableProps}>
-                  <span {...magic.dragHandleProps}>🔥</span>
-                  Two
-                  </li>)}
-              </Draggable> 
-            </ul>}
+            {(magic) => (
+            <Board ref={magic.innerRef} {...magic.droppableProps}>
+              {toDos.map((toDo, index) => <Draggable draggableId={toDo} index={index}>
+                {(magic) => (
+                  <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                    {toDo}
+                  </Card>)}
+              </Draggable>)}
+              {magic.placeholder}
+            </Board>)}
           </Droppable>
-        </div>
-      </DragDropContext> 
-    </>
+        </Boards>
+      </Wrapper>
+    </DragDropContext> 
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  bgColor: 'black',
-  textColor: 'black',
-  accentColor: 'black',
-  cardBgColor: 'black',
+  bgColor: '#3F8CF2',
+  boardColor: '#DADFE9',
+  cardColor: 'white'
 }
