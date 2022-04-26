bdc5e753f8d05eb9af3ee7cc372f8eb0ed615150
sangminlee98 <tkdals0975@naver.com>
2022-04-26 18:30:26 +0900

    Optimize performance using react-memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import {DragDropContext, Draggable, Droppable, DropResult} from 'react-beautiful-dnd';
+import {DragDropContext, Droppable, DropResult} from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import { toDoState } from './atoms';
+import DraggableCard from './Components/DraggableCard';
 
 const Wrapper = styled.div`
   display: flex;
@@ -26,12 +27,6 @@ const Board = styled.div`
   border-radius: 5px;
   min-height: 200px;
 `;
-const Card = styled.div`
-  background-color: ${props => props.theme.cardColor};
-  border-radius: 5px;
-  padding: 10px 10px;
-  margin-bottom: 5px;
-`;
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
@@ -51,12 +46,7 @@ function App() {
           <Droppable droppableId='one'>
             {(magic) => (
             <Board ref={magic.innerRef} {...magic.droppableProps}>
-              {toDos.map((toDo, index) => <Draggable draggableId={toDo} index={index} key={toDo}>
-                {(magic) => (
-                  <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
-                    {toDo}
-                  </Card>)}
-              </Draggable>)}
+              {toDos.map((toDo, index) => <DraggableCard key={toDo} toDo={toDo} index={index}/>)}
               {magic.placeholder}
             </Board>)}
           </Droppable>
