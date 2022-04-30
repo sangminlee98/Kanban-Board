67f6564e58612ec3c87681e859b3a613c7a11e32
sangminlee98 <tkdals0975@naver.com>
2022-04-30 12:52:27 +0900

    set card opacity dragging card

--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
-const Card = styled.div`
+const Card = styled.div<{isDragging: boolean}>`
   background-color: ${props => props.theme.cardColor};
   border-radius: 5px;
   padding: 10px 10px;
   margin-bottom: 5px;
+  opacity: ${props => props.isDragging ? '0.8' : 'none'};
 `;
 
 interface IDraggableCardProps {
@@ -16,8 +17,8 @@ interface IDraggableCardProps {
 const DraggableCard = ({toDo, index}: IDraggableCardProps) => {
   return (
     <Draggable draggableId={toDo} index={index}>
-      {(provided) => (
-        <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+      {(provided, snapshot) => (
+        <Card isDragging={snapshot.isDragging} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
           {toDo}
         </Card>)}
     </Draggable>
